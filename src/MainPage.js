32e9526cabbc4eec7703f1f1373da5e21233e03e
Tomasz Sułecki <t.sulecki95@gmail.com>
2017-06-24 22:47:54 +0200

    init

--- src/MainPage.js
+++ src/MainPage.js
@@ -8,7 +8,6 @@ class Main extends React.Component{
     render(){
         return (
             <div>
-
                 <div  className="bgimg-1"/>
 
                 <div id="portfolio" className="div1">
